1bf89bd37b896acaaab52104a7548c24adbb32d2
Areum Jo <areumjo1@gmail.com>
2020-11-15 19:43:20 -0800

    Now all maps/charts work functionally -- time to add tooltips and change the case number when you hover the line chart.

--- src/Components/Counties.js
+++ src/Components/Counties.js
@@ -1,99 +1,91 @@
 import React, { useState, useEffect } from "react";
 import { ComposableMap, Geographies, Geography, Marker, Annotation } from "react-simple-maps"
-import { geoCentroid } from "d3-geo";
 
 import { csv } from "d3-fetch";
-// import csvState from "../Data/Country/JHU_IncidentCases_State.csv"
-import { LineChart, Line, CartesianGrid, XAxis, YAxis, Tooltip, ReferenceLine, Label } from 'recharts';
+import csvCounty from "../Data/County/JHU_CumulativeCases_County.csv";
+import csvLocation from "../Data/locations_information.csv";
+import { scaleQuantile } from "d3-scale";
+import CountyChart from "./CountyChart";
 
-import allStates from "../Maps/allstates.json";
+const geoUrl = "https://cdn.jsdelivr.net/npm/us-atlas@3/counties-10m.json";
+const latestDate = 'October 31 2020';
 
-const geoUrl = "https://cdn.jsdelivr.net/npm/us-atlas@3/states-10m.json";
-
-const offsets = {
-    DC: [30, 25]
-};
-
-
+// const location = csv(csvLocation).then(loc => {
+//     let countyLoc = loc.filter(d => d.type === "county");
+    
+// })
 const Counties = () => {
 
+    const [countyData, setCountyData] = useState([]); // for mapping
+    const [onCountyId, setOnCountyId] = useState("53033"); // initialize king county data
+    const [allCountiesData, setAllCountiesData] = useState([]);
+    const [countyCase, setCountyCase] = useState("1231");
+    const [countyLocation, setCountyLocation] = useState([]); 
 
+    useEffect(() => {
+        csv(csvCounty).then(county => {
+            let co = county[county.length-1];
+            let converted = Object.keys(co).map(key => ({ key, cases: co[key]}));
+            setCountyData(converted);
+            // console.log(countyData) -- {key: "10001", cases: "1974.1572909406702"}
+            setAllCountiesData(county);
+        })
+    }, []);
+
+    useEffect(() => {
+        csv(csvLocation).then(loc => {
+            setCountyLocation(loc.filter(d => d.type === "county"));
+        })
+    }, []);
+
+    let countyName = countyLocation.filter(d => d.fips === onCountyId).map(d => `${d.name} in ${d.state}`);
+    // console.log('will>', countyName)
+
+    const colorScale = scaleQuantile()
+        .domain(countyData.map(d => d.cases))
+        .range([
+            "#ffedea",
+            "#ffcec5",
+            "#ffad9f",
+            "#ff8a75",
+            "#ff5533",
+            "#e2492d",
+            "#be3d26",
+            "#9a311f",
+            "#782618"
+        ]);
 
     return (
-        <div className="vis-wrapper">
-            {/* <div className="forecast usmap" >
-                <ComposableMap projection="geoAlbersUsa" 
-                projectionConfig={{ scale: 800 }} 
-                width={900}
-                style={{ width: "100%", height: "auto" }}>
-                    <Geographies geography={geoUrl}>
-                        {({ geographies }) => (
-                        <>
-                            {geographies.map(geo => (
-                            <Geography
-                                key={geo.rsmKey}
-                                stroke="#FFF"
-                                geography={geo}
-                                style={{
-                                        default: {
-                                            fill: "#DDD",
-                                            stroke: "#FFF",
-                                            strokeWidth: 1,
-                                            outline: "none",},
-                                        hover: {
-                                            fill: "#F5A51C",
-                                            stroke: "#FFF",
-                                            strokeWidth: 1,
-                                            outline: "none",},
-                                        pressed: {
-                                            fill: "#EE3E23",
-                                            stroke: "#EE3E23",
-                                            outline: "none",}}}/>
-                            ))}
-                            {geographies.map(geo => {
-                            const centroid = geoCentroid(geo);
-                            const cur = allStates.find(s => s.val === geo.id);
-                            return (
-                                <g key={geo.rsmKey + "-name"}>
-                                {cur &&
-                                    centroid[0] > -160 &&
-                                    centroid[0] < -67 &&
-                                    (Object.keys(offsets).indexOf(cur.id) === -1 ? (
-                                    <Marker coordinates={centroid}>
-                                        <text y="3" fontSize={12} textAnchor="middle">{cur.id}</text>
-                                    </Marker>
-                                    ) : (
-                                    <Annotation subject={centroid} dx={offsets[cur.id][0]} dy={offsets[cur.id][1]}>
-                                        <text x={2} fontSize={12} alignmentBaseline="middle">{cur.id}</text>
-                                    </Annotation>
-                                    ))}
-                                </g>
-                            );
-                            })}
-                        </>
-                        )}
-                    </Geographies>
-                </ComposableMap>
-            </div>
-             */}
-        {/* { setOnState && 
-        <div className="forecast">
-            <LineChart width={600} height={400} data={nationalData} margin={{ top: 10, right: 40, bottom: 40, left: 5 }}>
-                <Line type="monotone" dataKey="US" stroke="#043b4e" strokeWidth={4} />
-                <CartesianGrid stroke="#ccc" strokeDasharray="5 5" vertical={false} />
-                <XAxis dataKey="dates" fontSize="10" axisLine={false} tickLine={false} tickFormatter={formatXAxis} />
-                <YAxis fontSize="10" axisLine={false} tickLine={false} domain={[0, 200]}/>
-                <ReferenceLine x="2020-01-25" stroke="#EE3E23" strokeDasharray="4 4" label={{ position: "bottom", value: "First case in US", fontSize: "12", fill:"#EE3E23", offset: "20"  }} />
-                <ReferenceLine x="2020-03-14" stroke="#4A72B7" strokeDasharray="4 4" label={{ position: 'insideTopLeft',  value: 'National emergency declare', fill: '#4A72B7', fontSize: "12" }} />
-                <ReferenceLine x="2020-03-28" stroke="#809f3d" strokeDasharray="4 4" label={{ position: "bottom", value: "CARES act enacted", fill: "#809f3d", offset: "30", fontSize: "12" }} />          
-                <ReferenceLine x="2020-04-18" stroke="#F48620" strokeDasharray="4 4" label={{ position: 'bottom', value: "Stimulus payments starts", fill: "#F48620", offset: "45", fontSize: "12" }} />
-                <ReferenceLine x="2020-10-31" stroke="#368243" strokeDasharray="4 4" label={{ position: "bottom", value: "Election day", fill: "#368243", offset: "20", fontSize: "12" }} />
-                <Tooltip />
-            </LineChart>
+        <div>
+            <p style={{fontSize: "14px"}}>In the <code>{countyName}</code>, on <code>{latestDate}</code>, there were <code>{countyCase}</code> reported COVID-19 cases per 100K people.</p>
+            <div className="vis-wrapper">
+                <div className="forecast usmap" >
+                    <ComposableMap data-tip="" projection="geoAlbersUsa" projectionConfig={{ scale: 800 }} 
+                            width={900} style={{ width: "100%", height: "auto" }}>
+                        <Geographies geography={geoUrl}>
+                            {({ geographies }) =>
+                            geographies.map(geo => {
+                                const cur = countyData.find(s => s.key === geo.id);
+                                return (
+                                <Geography
+                                    key={geo.rsmKey}
+                                    geography={geo}
+                                    fill={cur ? colorScale(cur.cases) : "#EEE"}
+                                    onClick={() => {
+                                        console.log(cur.key, cur.cases);
+                                        setOnCountyId(cur.key);
+                                        setCountyCase(cur.cases);
+                                    }}
+                                />
+                                );
+                            })
+                            }
+                        </Geographies>
+                    </ComposableMap>
+                </div>
+            {allCountiesData && <CountyChart onCountyId={onCountyId} allCountiesData={allCountiesData} /> }
         </div>
-        } */}
-        <p> Counties compo</p>
-        </div>
+    </div>
     )
 }
 
