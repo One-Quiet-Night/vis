0d61f0c763b621bd92968ee7de0b535a0789faaa
Jae Cho <jaehun.cho@gmail.com>
2020-12-06 09:14:03 -0800

    Add 2020-12-06 forecast. Refactor configs.

--- src/Components/Forecast.js
+++ src/Components/Forecast.js
@@ -4,6 +4,8 @@ import States from "./States";
 import Counties from "./Counties";
 
 import ReactGA from 'react-ga';
+import config from "../config.json"
+
 
 const Forecast = () => {
 
@@ -20,7 +22,7 @@ const Forecast = () => {
 
     return (
         <div className="container">
-            <p className="text-height" style={{maxWidth: "900px", margin: "0 auto 30px auto", textAlign: "left"}}><code>One Quiet Night</code> develops scientifically-driven machine learning models to accurately predict the spread of COVID-19 infections. This generates accurate forecasts that are competitive with and different from the current set of models in the <a href="https://c3.ai/customers/covid-19-data-lake/" className="c3-ai-link">CDC COVID-19 ensemble</a>. 
+            <p className="text-height" style={{maxWidth: "900px", margin: "0 auto 30px auto", textAlign: "left"}}><code>One Quiet Night</code> develops scientifically-driven machine learning models to accurately predict the spread of COVID-19 infections. This generates accurate forecasts that are competitive with and different from the current set of models in the <a href="https://c3.ai/customers/covid-19-data-lake/" className="c3-ai-link">CDC COVID-19 ensemble</a>.
             We operationalize the forecast to retrain the model and make predictions on new data.</p>
             <p className="text-height" style={{maxWidth: "900px", margin: "0 auto 30px auto", textAlign: "left"}}>Our historical forecasts can be found on <a className="c3-ai-link" href="https://viz.covid19forecasthub.org/">COVID-19 Forecast Hub</a>.</p>
             <div className="options">
@@ -50,7 +52,7 @@ const Forecast = () => {
                     isNational ? <National /> : isState ? <States /> : <Counties />
                 }
             </div>
-            <p className="text-height" style={{maxWidth: "500px", fontSize: "12px", margin: "30px auto 0 auto"}}>Weekly data last updated: 2020-11-29</p>
+            <p className="text-height" style={{maxWidth: "500px", fontSize: "12px", margin: "30px auto 0 auto"}}>Weekly data last updated: {config.dataEndDate}</p>
             <p className="text-height" style={{maxWidth: "500px", fontSize: "12px", margin: "15px auto 0 auto"}}><code>One Quiet Night</code> uses data from <a href="https://c3.ai/customers/covid-19-data-lake/" className="c3-ai-link">C3 AI COVID-19 Data Lake</a> and visualizes weekly COVID-19 cases and 4 weeks forecast from our forecasting model.</p>
         </div>
     )
